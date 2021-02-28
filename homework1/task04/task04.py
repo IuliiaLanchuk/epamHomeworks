d998d0a0f6109ea022d7e3e1c50bceb8c73b65b0
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-02-28 14:36:41 +0300

    add task04

---
+++ homework1/task04/task04.py
@@ -0,0 +1,25 @@
+from itertools import product
+from typing import List
+
+
+def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+    lst = list(product(a, b, c, d))
+    counter = 0
+
+    for i in lst:
+        if i[0] + i[1] + i[2] + i[3] == 0:
+            counter += 1
+            # print(i)
+    return counter
+
+
+a = [1, -2, 3, 4, -5]
+b = [6, 7, 2, 9, 5]
+c = [-10, 12, -13, 14, 15]
+d = [16, 17, 13, 19, 20]
+print(check_sum_of_four(a, b, c, d))  # returns 4
+
+# (-2, 2, -13, 13)
+# (-5, 2, -10, 13)
+# (-5, 2, -13, 16)
+# (-5, 5, -13, 13)
